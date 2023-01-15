0ab692012dfe5cca0160af0d00004eba5303419e
Leonardo Caballero <leonardocaballero25@gmail.com>
2023-01-15 15:20:10 -0600

    first commit

---
+++ src/formulario/calculadora/FormularioCalculadora.java
@@ -0,0 +1,18 @@
+
+package formulario.calculadora;
+
+
+public class FormularioCalculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+      CALCULADORA ventana=new CALCULADORA();
+        ventana.setVisible(true);//se muestra el formulario
+       
+      ventana.setLocationRelativeTo(null);//se centra el formulario   
+    }
+    
+}
